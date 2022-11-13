c4034d2bae16c8a566a7289ca77048ae33e8a4a5
iDeboy <Theboygamer77@gmail.com>
2022-11-13 14:10:15 -0600

    Corrección rápida

--- SimuladorCPU/Modelos/PlanificadorFiFo.cs
+++ SimuladorCPU/Modelos/PlanificadorFiFo.cs
@@ -6,42 +6,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimuladorCPU.Modelos
-{
-    public class PlanificadorFiFo : IEnumerable<ProcesosFiFo>
-    {
+namespace SimuladorCPU.Modelos {
+    public class PlanificadorFiFo : IEnumerable<ProcesosFiFo> {
         private List<ProcesosFiFo> procesos;
         public bool Finalizado { get; private set; } = false;
-        public PlanificadorFiFo()
-        {
+        public PlanificadorFiFo() {
             procesos = new List<ProcesosFiFo>();
         }
 
         public void Add(ProcesosFiFo p) {
 
-            if (Finalizado)
-            {
+            if (Finalizado) {
                 return;
             }
             procesos.Add(p);
             procesos = this.OrderBy(p => p.TiempoLlegada).ToList();
         }
-        public IEnumerator<ProcesosFiFo> GetEnumerator()
-        {
+        public IEnumerator<ProcesosFiFo> GetEnumerator() {
             return ((IEnumerable<ProcesosFiFo>)procesos).GetEnumerator();
         }
-        public void Clear()
-        {
+        public void Clear() {
+            Finalizado = false;
             procesos.Clear();
         }
-        IEnumerator IEnumerable.GetEnumerator()
-        {
+        IEnumerator IEnumerable.GetEnumerator() {
             return ((IEnumerable)procesos).GetEnumerator();
         }
-        public void Finalizar()
-        {
-            if (!this.Any())
-            {
+        public void Finalizar() {
+            if (!this.Any()) {
                 return;
             }
             Finalizado = true;
@@ -49,15 +41,14 @@ namespace SimuladorCPU.Modelos
             primero.TiempoFinal = primero.TiempoSalida;
             primero.TiempoEspera = 0;
             primero.TiempoRespuesta = primero.TiempoSalida;
-            
-            for(int i = 1; i < procesos.Count; i++)
-            {
-                procesos[i].TiempoFinal = procesos[i].TiempoSalida + procesos[i - 1].TiempoFinal;
+
+            for (int i = 1; i < procesos.Count; i++) {
+                procesos[i].TiempoFinal = procesos[i].TiempoSalidaBloqueado + procesos[i - 1].TiempoFinal;
                 procesos[i].TiempoEspera = procesos[i - 1].TiempoRespuesta - procesos[i].TiempoLlegada;
                 procesos[i].TiempoRespuesta = procesos[i].TiempoSalida + procesos[i - 1].TiempoRespuesta;
             }
-                
-            
+
+
         }
     }
 }
